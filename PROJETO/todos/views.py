bf8756cff4560e59182bed07c32b47717b15b2c6
Helio Mattos <heliomatto@gmail.com>
2025-03-11 19:08:26 -0300

    sexto commit

---
+++ PROJETO/todos/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Todo
+
+
+# Create your views here.
+def todo_list_old(request): 
+    nome = 'Rodrigo' 
+    alunos = ['1. Ana', '2. José', '3. Bia'] 
+    return render(request, "todos/todo_list.html", {"nome": nome, "lista_alunos": alunos})
+
+def todo_list(request): 
+    todos = Todo.objects.all()
+    return render(request, "todos/todo_list.html", {"todos":todos});
